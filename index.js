6f32756679c43b55fb77f45417d0694cc67d22e1
hotaru <huyuchen0810@gmail.com>
2023-12-07 18:14:30 +0900

    update 3.8

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express= require(`express`);
 const app = express();
 app.use(express.json());
 
+const morgan = require(`morgan`);
+ morgan.token(`payload`, function(req){
+  return (
+    (req.method === `POST` && req.body.name)
+    ? JSON.stringify(req.body)
+    : null
+  )
+})
+
+const middleWare = morgan(`:method :url :status :res[content-length] - :response-time ms :req[header] :payload`);
+app.use(middleWare);
+
 let notes=[
     { 
       "id": 1,
@@ -26,9 +38,9 @@ let notes=[
     {
         "id":5,
         "name": "test",
-        "number": "00-00-1112223"
+        "number": "00-00-111222"
     }
-]
+];
 
 app.get("/",(request, response)=>{
 response.send(`<h1>Phonebook</h1>`)
@@ -39,7 +51,7 @@ response.json(notes);
 })
 
 app.get(`/info`, (request, response)=>{
-    const date= new Date();
+    const date= new Date().toString();
 response.send( `
 <p>Phonebook has info for ${notes.length} people</p>
 <br/>
@@ -50,8 +62,46 @@ response.send( `
 app.get(`/api/persons/:id`, (request, response)=>{
 const id = Number(request.params.id);
 const matchednote = notes.find(note => note.id === id);
-matchednote ? response.json(notes[id-1]) : response.status(400).end()
+matchednote ? response.json(matchednote) : response.status(400).end()
 })
+
+const generatedNewId = () =>{
+  const maxId = notes.length > 0
+  ?  Math.max(...notes.map(note => note.id))
+  : 0;
+  return maxId + 1
+  }
+
+app.post(`/api/persons`, (request, response)=>{
+  const body = request.body;
+
+const newNote ={
+id: generatedNewId(),
+name: body.name,
+number: body.number 
+}
+
+const exsitingName = notes.find(note => note.name === body.name)
+if(exsitingName){
+  return response.status(400).json({error: `name must be unique`})
+}
+if(!body.number){
+  return response.status(400).json({error: `number missing`})
+}
+if(!body.name){
+  return response.status(400).json({error: `name missing`})
+}
+notes = notes.concat(newNote);
+newNote ? response.json(newNote) : response.status(400).end()
+}
+)
+
+
+app.delete(`/api/persons/:id`, (request,response)=>{
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+  response.status(204).end();
+  })
 
 const PORT = 3005
 app.listen((PORT), () =>{
