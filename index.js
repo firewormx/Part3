634eb0c5dc1351c8e13a6e4d5520f239c40acb20
hotaru <huyuchen0810@gmail.com>
2024-02-15 18:08:34 +0900

    update Part3-for phonebook

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+// Part3 - 3.1-3.8
 const express= require(`express`);
 const app = express();
+const cors = require(`cors`);
+
 app.use(express.json());
+app.use(cors());
+app.use(express.static(`dist`));
 
 const morgan = require(`morgan`);
  morgan.token(`payload`, function(req){
@@ -75,13 +80,7 @@ const generatedNewId = () =>{
 app.post(`/api/persons`, (request, response)=>{
   const body = request.body;
 
-const newNote ={
-id: generatedNewId(),
-name: body.name,
-number: body.number 
-}
-
-const exsitingName = notes.find(note => note.name === body.name)
+const exsitingName = notes.find(note => note.name === body.name);
 if(exsitingName){
   return response.status(400).json({error: `name must be unique`})
 }
@@ -91,10 +90,32 @@ if(!body.number){
 if(!body.name){
   return response.status(400).json({error: `name missing`})
 }
+
+const newNote ={
+  id: generatedNewId(),
+  name: body.name,
+  number: body.number 
+  }
+
 notes = notes.concat(newNote);
 newNote ? response.json(newNote) : response.status(400).end()
 }
 )
+
+app.put(`/api/persons/:id`,(request, response)=>{
+ const id = Number(request.params.id);
+ console.log(id);
+ const body = request.body;
+
+ const changedPerson = {
+  name: body.name,
+  number: body.number,
+  id: body.id
+ }
+notes= notes.map(note => note.id !== id ? note :  changedPerson);
+response.json(changedPerson);
+
+})
 
 
 app.delete(`/api/persons/:id`, (request,response)=>{
@@ -103,7 +124,7 @@ app.delete(`/api/persons/:id`, (request,response)=>{
   response.status(204).end();
   })
 
-const PORT = 3005
+const PORT = 3007
 app.listen((PORT), () =>{
     console.log(`Server running on port ${PORT}`)
 })
